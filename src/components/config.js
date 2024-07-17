48a3690f91b192e115b7455650d1208eecc0be31
jaswanthte <jaswanth.sabbineni@techenhance.com>
2024-07-17 19:44:23 +0530

    Update config.js

--- src/components/config.js
+++ src/components/config.js
@@ -1,15 +1,11 @@
-require('dotenv').config();
-
-const avatarAppConfig = {
-  cogSvcRegion: process.env.COG_SVC_REGION,
-  cogSvcSubKey: process.env.COG_SVC_SUB_KEY,
-  voiceName: process.env.VOICE_NAME,
-  avatarCharacter: process.env.AVATAR_CHARACTER,
-  avatarStyle: process.env.AVATAR_STYLE,
-  avatarBackgroundColor: process.env.AVATAR_BACKGROUND_COLOR,
-  iceUrl: process.env.ICE_URL,
-  iceUsername: process.env.ICE_USERNAME,
-  iceCredential: process.env.ICE_CREDENTIAL
+export const avatarAppConfig = {
+  cogSvcRegion: process.env.REACT_APP_COG_SVC_REGION,
+  cogSvcSubKey: process.env.REACT_APP_COG_SVC_SUB_KEY,
+  voiceName: process.env.REACT_APP_VOICE_NAME,
+  avatarCharacter: process.env.REACT_APP_AVATAR_CHARACTER,
+  avatarStyle: process.env.REACT_APP_AVATAR_STYLE,
+  avatarBackgroundColor: process.env.REACT_APP_AVATAR_BACKGROUND_COLOR,
+  iceUrl: process.env.REACT_APP_ICE_URL,
+  iceUsername: process.env.REACT_APP_ICE_USERNAME,
+  iceCredential: process.env.REACT_APP_ICE_CREDENTIAL
 };
-
-console.log(avatarAppConfig);
