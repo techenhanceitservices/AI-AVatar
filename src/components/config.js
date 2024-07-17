c906b0bece4ffc0a4525c50bf352d1d55b6c9b2d
jaswanthte <jaswanth.sabbineni@techenhance.com>
2024-07-17 19:34:16 +0530

    Update config.js

--- src/components/config.js
+++ src/components/config.js
@@ -1,11 +1,15 @@
-export const avatarAppConfig = {
-  cogSvcRegion : "westus2",
-  cogSvcSubKey : "e23a5f4ee49348b397b2ade82e388055",
-  voiceName : "en-US-JennyNeural",
-  avatarCharacter : "lisa",
-  avatarStyle : "casual-sitting",
-  avatarBackgroundColor : "#FFFFFFFF",
-  iceUrl : "stun:global.stun.twilio.com:3478",
-  iceUsername : "bbc7d2f8f92f9ed545f6b58ba968fb8bf9c19aeeb7901f1534a91b15b89d7a49",
-  iceCredential : "0oWBOOw2/g5KMy1Vcrq9uU4t8mlXqe03wkPIITOb3U="
-}
+require('dotenv').config();
+
+const avatarAppConfig = {
+  cogSvcRegion: process.env.COG_SVC_REGION,
+  cogSvcSubKey: process.env.COG_SVC_SUB_KEY,
+  voiceName: process.env.VOICE_NAME,
+  avatarCharacter: process.env.AVATAR_CHARACTER,
+  avatarStyle: process.env.AVATAR_STYLE,
+  avatarBackgroundColor: process.env.AVATAR_BACKGROUND_COLOR,
+  iceUrl: process.env.ICE_URL,
+  iceUsername: process.env.ICE_USERNAME,
+  iceCredential: process.env.ICE_CREDENTIAL
+};
+
+console.log(avatarAppConfig);
