2d13b966c6266d16511bbb84905c1019ce8bffa0
jaswanthte <jaswanth.sabbineni@techenhance.com>
2024-07-17 19:24:24 +0530

    Add files via upload
    Need to integrate Chatgpt

---
+++ src/components/config.js
@@ -0,0 +1,11 @@
+export const avatarAppConfig = {
+  cogSvcRegion : "westus2",
+  cogSvcSubKey : "e23a5f4ee49348b397b2ade82e388055",
+  voiceName : "en-US-JennyNeural",
+  avatarCharacter : "lisa",
+  avatarStyle : "casual-sitting",
+  avatarBackgroundColor : "#FFFFFFFF",
+  iceUrl : "stun:global.stun.twilio.com:3478",
+  iceUsername : "bbc7d2f8f92f9ed545f6b58ba968fb8bf9c19aeeb7901f1534a91b15b89d7a49",
+  iceCredential : "0oWBOOw2/g5KMy1Vcrq9uU4t8mlXqe03wkPIITOb3U="
+}
